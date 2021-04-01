d77abcaec85822caad82f5c359db16ab2be73794
Deepak <delta12kilo@users.noreply.github.com>
2021-04-01 21:22:44 +0530

    Update template.py
    Remove unessary imports.

--- regression/template.py
+++ regression/template.py
@@ -1,14 +1,12 @@
 import os 
-import numpy as np
-import pandas as pd
-import seaborn as sns
-import plotly.express as px
-import matplotlib.pyplot as plt 
+# import seaborn as sns
+# import plotly.express as px
+# import matplotlib.pyplot as plt 
 
-from sklearn.preprocessing import StandardScaler
-from sklearn.impute import SimpleImputer
-from sklearn.preprocessing import OneHotEncoder, LabelEncoder
-from sklearn.model_selection import train_test_split
+# from sklearn.preprocessing import StandardScaler
+# from sklearn.impute import SimpleImputer
+# from sklearn.preprocessing import OneHotEncoder, LabelEncoder
+# from sklearn.model_selection import train_test_split
 import pickle
 
 def save_model(obj,path_to_save):
